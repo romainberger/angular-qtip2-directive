4196af17e1fabc95351ae9a93e6bab1fa09cb31c
Romain Berger <romain@romainberger.com>
2014-01-15 23:37:32 +0100

    Remove useless variables + iife

--- qtip2.js
+++ qtip2.js
@@ -1,24 +1,30 @@
-angular.module('qtip2', [])
-  .directive('qtip', function($rootScope, $timeout, $window) {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-        var my = attrs.my || 'bottom center'
-          , at = attrs.at || 'top center'
+!function($) {
 
-        element.qtip({
-          content: attrs.content,
-          position: {
-            my: my,
-            at: at,
-            target: element
-          },
-          hide: {
-            fixed : true,
-            delay : 100
-          },
-          style: 'qtip'
-        })
+  'use strict';
+
+  angular.module('qtip2', [])
+    .directive('qtip', function() {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          var my = attrs.my || 'bottom center'
+            , at = attrs.at || 'top center'
+
+          $(element).qtip({
+            content: attrs.content,
+            position: {
+              my: my,
+              at: at,
+              target: element
+            },
+            hide: {
+              fixed : true,
+              delay : 100
+            },
+            style: 'qtip'
+          })
+        }
       }
-    }
-  })
+    })
+
+}(window.jQuery);
