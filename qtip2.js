e24c2b06d2147c988fdcbd576bc7171dac610fae
noamokman <noamokman@gmail.com>
2014-05-17 12:39:14 +0300

    added ; at necessary locations

--- qtip2.js
+++ qtip2.js
@@ -2,30 +2,30 @@
 
   'use strict';
 
-  angular.module('qtip2', [])
-    .directive('qtip', function() {
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-          var my = attrs.my || 'bottom center'
-            , at = attrs.at || 'top center'
-            , qtipClass = attrs.class || 'qtip'
+    angular.module('qtip2', [])
+        .directive('qtip', function() {
+            return {
+                restrict: 'A',
+                link: function(scope, element, attrs) {
+                    var my = attrs.my || 'bottom center',
+                        at = attrs.at || 'top center',
+                        qtipClass = attrs.class || 'qtip';
 
-          $(element).qtip({
-            content: attrs.content,
-            position: {
-              my: my,
-              at: at,
-              target: element
-            },
-            hide: {
-              fixed : true,
-              delay : 100
-            },
-            style: qtipClass
-          })
-        }
-      }
-    })
+                    $(element).qtip({
+                        content: attrs.content,
+                        position: {
+                            my: my,
+                            at: at,
+                            target: element
+                        },
+                        hide: {
+                            fixed: true,
+                            delay: 100
+                        },
+                        style: qtipClass
+                    });
+                }
+            };
+        });
 
 }(window.jQuery);
